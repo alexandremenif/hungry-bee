128fdc659b2b547f1ea909ae88c92faba201a131
Alexandre Menif <alexandre.menif@decisionbrain.com>
2023-10-27 23:54:04 +0200

    Setup authentication with Google provider

---
+++ src/app/sign-in/sign-in.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Auth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from '@angular/fire/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
+})
+export class SignInComponent {
+
+  readonly provider = new GoogleAuthProvider();
+
+  constructor(readonly auth: Auth, readonly router: Router) {}
+
+  async signIn() {
+    await signInWithPopup(this.auth, this.provider);
+    await this.router.navigate(['/']);
+  }
+}
