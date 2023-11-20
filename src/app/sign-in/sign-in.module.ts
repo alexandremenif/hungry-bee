f3ef7fee6a21c7113a89fe0231cee7fe6fbda843
Alexandre Menif <alexandre.menif@decisionbrain.com>
2023-11-20 22:36:30 +0100

    Use lazy-loading modules to reduce main bundle size

---
+++ src/app/sign-in/sign-in.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SignInRoutingModule } from './sign-in-routing.module';
+import { SignInComponent } from './sign-in.component';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [SignInComponent],
+  imports: [
+    CommonModule,
+    SignInRoutingModule,
+    MatButtonModule
+  ]
+})
+export class SignInModule { }
